54bfdf494ac83cdc90d22da5ece0f409d12a80eb
meganerd1220 <123340494+meganerd1220@users.noreply.github.com>
2024-07-31 17:09:44 -0500

    Add playGame function to keep track of scores across 5 rounds

--- myscripts.js
+++ myscripts.js
@@ -30,15 +30,12 @@ function getHumanChoice() { // returns "rock", "paper", or "scissors"
 
     switch (userChoice) {
         case "rock":
-            alert("You chose Rock");
             console.log("hc: rock");
             return "rock";
         case "paper":
-            alert("You chose Paper");
             console.log("hc: paper");
             return "paper";
         case "scissors":
-            alert("You chose Scissors");
             console.log("hc: scissors");
             return "scissors";
         default:
@@ -52,49 +49,58 @@ function playRound(computer, human) { // params: computer choice, human choice; 
     switch (computer) {
         case "rock":
             if (human == "paper") {
-                alert("Paper covers Rock. You win!");
+                alert(`You chose Paper. The computer chose Rock.
+                Paper covers Rock. You win!`);
                 console.log("cc: rock - hc: paper - human win");
                 return "human";
             }
             else if (human == "scissors") {
-                alert("Rock crushes Scissors. You lose.");
+                alert(`You chose Scissors. The computer chose Rock.
+                Rock crushes Scissors. You lose`);
                 console.log("cc: rock - hc: scissors - computer win");
                 return "computer";
             }
             else { // human == "rock"
-                alert("Tie");
+                alert(`You chose Rock. The computer chose Rock.
+                Tie`);
                 console.log("cc: rock - hc: rock - tie");
                 return "tie";
             }
         case "paper":
             if (human == "rock") {
-                alert("Paper covers Rock. You lose.");
+                alert(`You chose Rock. The computer chose Paper.
+                Paper covers Rock. You lose.`);
                 console.log("cc: paper - hc: rock - computer win");
                 return "computer";
             }
             else if (human == "scissors") {
-                alert("Scissors cut Paper. You win!");
+                alert(`You chose Scissors. The computer chose Paper.
+                Scissors cut Paper. You win!`);
                 console.log("cc: paper - hc: scissors - human win");
                 return "human";
             }
             else { // human == "paper"
-                alert("Tie");
+                alert(`You chose Paper. The computer chose Paper.
+                Tie`);
                 console.log("cc: paper - hc: paper - tie");
                 return "tie";
             }
         case "scissors":
             if (human == "rock") {
-                alert("Rock crushes Scissors. You win!");
+                alert(`You chose Rock. The computer chose Scissors.
+                Rock crushes Scissors. You win!`);
                 console.log("cc: scissors - hc: rock - human win");
                 return "human";
             }
             else if (human == "paper") {
-                alert("Scissors cut Paper. You lose.");
+                alert(`You chose Paper. The computer chose Scissors.
+                Scissors cut Paper. You lose.`);
                 console.log("cc: scissors - hc: paper - computer win");
                 return "computer";
             }
             else { // human == "scissors"
-                alert("Tie");
+                alert(`You chose Scissors. The computer chose Scissors.
+                Tie`);
                 console.log("cc: scissors - hc: scissors - tie");
                 return "tie";
             }
@@ -104,13 +110,42 @@ function playRound(computer, human) { // params: computer choice, human choice; 
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-let computer = getComputerChoice();
-    console.log("global computer:", computer);
-    let human = getHumanChoice();
-    console.log("global human:", human);
-let winner = playRound(computer, human);
-console.log("winner: ", winner);
+function playGame() {
+    const ROUNDS = 5;
+    let currentRound = 1;
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < ROUNDS; i++) {
+        let winner = playRound(getComputerChoice(), getHumanChoice());
+        console.log(`Round ${currentRound} winner: `, winner);
+    
+        switch (winner) {
+            case "computer":
+                computerScore += 1;
+                console.log("cs: ", computerScore, "hs: ", humanScore);
+                break;
+            case "human":
+                humanScore += 1;
+                console.log("cs: ", computerScore, "hs: ", humanScore);
+                break;
+            case "tie":
+                computerScore += 1;
+                humanScore += 1;
+                console.log("cs: ", computerScore, "hs: ", humanScore);
+                break;
+            default:
+                console.log("playRound return is broken");
+                break;
+        }
+    }
+    console.log("Out of for loop - cs: ", computerScore, "hs: ", humanScore);
+    alert(`Final Score: 
+    Computer: ${computerScore}
+    Human: ${humanScore}`);
+}
+
+playGame();
+
+
 
 
