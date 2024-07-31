5fb7e1bf6c579cde98f1408fa4b1ca450d3c9e02
meganerd1220 <123340494+meganerd1220@users.noreply.github.com>
2024-07-31 15:23:15 -0500

    Add working getComputerChoice function

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,21 @@
-console.log("Connected!");
+console.log("Connected to HTML!");
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 300);
+    console.log(choice);
+    
+    if (choice < 100) {// 0-99
+        console.log("rock");
+        return "rock";
+    } else if (choice <= 200 && choice >= 100) { // 100-200
+            console.log("paper");
+            return "paper";
+    } else if (choice >= 201) { // 201-300
+            console.log("scissors");
+            return "scissors";
+    } else {
+        console.log("problem");
+    }
+}
+
+getComputerChoice();
