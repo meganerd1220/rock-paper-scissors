58aebe5c254d49c7706f58ce9b52bfd16bc21cd7
meganerd1220 <123340494+meganerd1220@users.noreply.github.com>
2024-07-31 15:56:46 -0500

    Add playRound with logic for a tie

--- myscripts.js
+++ myscripts.js
@@ -5,13 +5,13 @@ function getComputerChoice() {
     console.log(choice);
     
     if (choice < 100) {// 0-99
-        console.log("rock");
+        console.log("cc: rock");
         return "rock";
     } else if (choice <= 200 && choice >= 100) { // 100-200
-            console.log("paper");
+            console.log("cc: paper");
             return "paper";
     } else if (choice >= 201) { // 201-300
-            console.log("scissors");
+            console.log("cc: scissors");
             return "scissors";
     } else {
         console.log("problem");
@@ -24,28 +24,44 @@ function getHumanChoice() {
     2. Paper
     3. Scissors`);
 
-    console.log(userInput);
+    console.log("userInput: ", userInput);
 
     switch (userInput) {
         case "1":
             alert("You chose Rock");
+            console.log("hc: rock");
             return "rock";
         case "2":
             alert("You chose Paper");
+            console.log("hc: paper");
             return "paper";
         case "3":
             alert("You chose Scissors");
+            console.log("hc: scissors");
             return "scissors";
         default:
             alert("Your response was not an option or formatted incorrectly. Please try again.");
             return getHumanChoice();
     }
+}
 
+function playRound() {
+    let computer = getComputerChoice();
+    let human = getHumanChoice();
+
+    if (computer == human) {
+        alert("Tie");
+    }
+    else if {
+        alert("There's a winner");
+    }
+    else {
+        alert("Missing case");
+    }
 }
 
 let humanScore = 0;
 let computerScore = 0;
-let computer = getComputerChoice();
-let human = getHumanChoice();
+playRound();
 
 
